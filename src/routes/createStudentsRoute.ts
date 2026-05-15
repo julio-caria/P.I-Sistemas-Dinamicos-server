e474091e58af49f6377080521d528e7a5e6242e2
Júlio Caria <julio.caria@hotmail.com>
2026-05-14 22:52:11 -0300

    feat: add createStudentRoute

---
+++ src/routes/createStudentsRoute.ts
@@ -0,0 +1,50 @@
+import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import { checkRequestJWT } from "./hooks/check-request-jwt.ts";
+import z from "zod";
+import { getUserFromRequest } from "../utils/getUserFromRequest.ts";
+import { eq } from "drizzle-orm";
+import { db } from "../database/client.ts";
+import { alunos } from "../database/schema.ts";
+
+export const createStudentRoute: FastifyPluginAsyncZod = async (server) => {
+  server.post("/api/students", { 
+    preHandler: [
+      checkRequestJWT,
+    ],
+    schema: {
+      tags: ["Student"],
+      description: "Create an enrollment to student",
+      summary: "",
+      response: {
+        201: z.object({
+          studentId: z.string(),
+        }),
+        401: z.object({
+          message: z.string(),
+        }),
+      },
+      body: z.object({
+        nome: z.string(),
+        cpf: z.number(),
+        email: z.email(),
+        telefone: z.number(),
+        endereco: z.string(),
+        ra: z.string(),
+        dataIngresso: z.date(),
+      })
+    }
+  }, async (request, reply) => {
+    const user = getUserFromRequest(request);
+
+    if(!user) {
+      return reply.status(401).send({ message: "You are'n authorized to use this resource"})
+    }
+
+    const result = await db
+      .select()
+      .from(alunos)
+      .where(eq(alunos.id, user.sub));
+
+    return reply.send({ studentId: result[0].id });
+  })
+}
